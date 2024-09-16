a022997a5bb51f636d41507d9b63d82fbc394a54
Hamza-Ashraf71 <mujahidhamza935@gmail.com>
2024-09-17 00:07:44 +0500

    push the code

---
+++ Python/Lecture 5/practice.py
@@ -0,0 +1,20 @@
+# #print numbers from 1 to 100
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# #print no from 100 to 1
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+#print the multipication table of level n.
+n = int(input("enter number"))
+i = 1
+while i <= 10:
+    print(n*1)
+    i += 1
