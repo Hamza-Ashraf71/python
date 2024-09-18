d9426947f9bfa1e7c94242b40f867acc22ae0178
Hamza-Ashraf71 <mujahidhamza935@gmail.com>
2024-09-18 21:50:45 +0500

    lec 5 complete

--- Python/Lecture 5/practice.py
+++ Python/Lecture 5/practice.py
@@ -55,17 +55,52 @@
 
 #Search for a number x from tuple using for loop.
 
-nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49)
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49)
 
-x = 49
-i = 0
-for el in nums:
-    if(el == x):
-        print("number found at idx", i)
+# x = 49
+# i = 0
+# for el in nums:
+#     if(el == x):
+#         print("number found at idx", i)
 
-    i += 1
+#     i += 1
 
+
+#print 1 to 100 numbers using range.
+
+# for i in range(1, 101):
+#     print(i)
        
+#print 100 to 1 numbers using range.
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+# print the table of n
+
+# num = int(input("enter number :"))
+
+# for i in range(1, 11):
+#     print(num * i)
 
 
+#WAP to print the sum of n natural numbers using while loop.
 
+n = 9
+sum = 0
+i = 1
+
+while i <= n:
+    sum += i
+    i += 1
+print("sum of numbers :", sum)
+
+#WAP to find the factorial of first n numbers using for loop.
+
+n = 5
+fact = 1
+
+for i in range(1, n+1):
+    fact *= i
+
+print("Factorial :", fact)
