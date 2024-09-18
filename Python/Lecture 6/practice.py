50c58f832828c749ece5ae41f8dd9d108df3b1a6
Hamza-Ashraf71 <mujahidhamza935@gmail.com>
2024-09-19 00:48:38 +0500

    functions

---
+++ Python/Lecture 6/practice.py
@@ -0,0 +1,41 @@
+#-----------------------Functions and Recursions---------------
+
+#WAF to print the length of the list.
+# cities = ["Islamabad","Lahore", "Quetta", "Peshawar", "Karachi"]
+# animals = ["cow", "buffalo", "goat", "ox"]
+
+# def print_len(list):
+#     print(len(list))
+
+# print_len(cities)
+# print_len(animals)
+
+# #WAF to print the item of the list in single line.
+# cities = ["Islamabad","Lahore", "Quetta", "Peshawar", "Karachi"]
+# animals = ["cow", "buffalo", "goat", "ox"]
+
+# def print_list(list):
+#     for item in list:
+#         print(item, end=" ")
+
+# print_list(cities)
+# print_list(animals)
+
+#WAF to find the factorial of n.
+
+def cal_fact(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(fact)
+
+cal_fact(9)
+
+#WAF to convert USD to RS.
+
+def converter(usd_val):
+    pkr_val = usd_val * 278
+    print(usd_val, "USD", pkr_val, "PKR")
+
+converter(73)
+
