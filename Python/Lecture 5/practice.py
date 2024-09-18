7e5ff78cd19672cff74dd614347e34ae0649499d
Hamza-Ashraf71 <mujahidhamza935@gmail.com>
2024-09-18 20:05:10 +0500

    lec 5

--- Python/Lecture 5/practice.py
+++ Python/Lecture 5/practice.py
@@ -13,8 +13,59 @@
 
 
 #print the multipication table of level n.
-n = int(input("enter number"))
-i = 1
-while i <= 10:
-    print(n * i)
+# n = int(input("enter number"))
+# i = 1
+# while i <= 10:
+#     print(n * i)
+#     i += 1
+
+#Print the following list using a loop.
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# i = 0
+
+# while i < len(nums):
+#     print(nums[i])
+#     i += 1
+
+    #Search for a number x from tuple using loop.
+
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+
+# x = 36    
+# i = 0 #initilaization
+
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found at idx", i )
+#     else:
+#         print("finding...")
+
+#     i += 1    
+        
+
+#print the elements of the following list using the for loop
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# for el in nums:
+#     print(el)
+
+
+
+#Search for a number x from tuple using for loop.
+
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49)
+
+x = 49
+i = 0
+for el in nums:
+    if(el == x):
+        print("number found at idx", i)
+
     i += 1
+
+       
+
+
+
